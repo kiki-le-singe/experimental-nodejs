0453dbef8990cc2e37fdef23d80fbf7ef00f8a3e
kiki le singe <aalbertini95@gmail.com>
2015-12-18 19:34:54 +0100

    feat(import_own_modules): implement import_own_modules

---
+++ server/modules_experiments/import_own_modules.js
@@ -0,0 +1,8 @@
+import math, { add, sub, mult } from 'en-math';
+import hello from 'en-hello';
+
+console.log(hello());
+console.log(math);
+console.log(add(1, 2));
+console.log(sub(2, 1));
+console.log(mult(2, 2));
---
+++ server/modules_experiments/index.js
@@ -0,0 +1,3 @@
+// enable runtime transpilation to use ES6/7 in node
+require('babel-register');
+require('./import_own_modules.js');
