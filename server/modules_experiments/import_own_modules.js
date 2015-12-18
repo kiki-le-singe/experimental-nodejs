c1ac0f3d8fe9be258ef263f108a1b5767cd67ecf
kiki le singe <aalbertini95@gmail.com>
2015-12-18 22:36:19 +0100

    feat(import_own_modules): show a global and local variables from a module

--- server/modules_experiments/import_own_modules.js
+++ server/modules_experiments/import_own_modules.js
@@ -1,6 +1,9 @@
 import math, { add, sub, mult } from 'en-math';
 import hello from 'en-hello';
+import { bar } from 'en-variables';
 
+console.log(bar); // display a local variable from `en-variables` module
+console.log(foo); // display a global variable from `en-variables` module
 console.log(hello());
 console.log(math);
 console.log(add(1, 2));
